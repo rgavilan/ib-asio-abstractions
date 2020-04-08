37f304f9018c21d0ff4a3fdbd203eff6b080f6e4
Alejandro Martino Uria <amartino@izertis.com>
2020-04-08 12:19:12 +0200

    SEMANTMURC-83  initial commit

---
+++ src/main/java/es/um/asio/abstractions/Constants.java
@@ -0,0 +1,10 @@
+package es.um.asio.abstractions;
+
+/**
+ * The Class Constants.
+ */
+public final class Constants {
+		
+	/** The Constant ROOT_URI. */
+	public static final String ROOT_URI = "http://example.org";
+}
