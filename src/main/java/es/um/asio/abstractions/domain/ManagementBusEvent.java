985b8b8afef080d4c9639ab21ffe985265b4d6f2
Alejandro Martino Uria <amartino@izertis.com>
2020-04-11 18:44:46 +0200

    SEMANTMURC-83  ManagementBusEvent

---
+++ src/main/java/es/um/asio/abstractions/domain/ManagementBusEvent.java
@@ -0,0 +1,22 @@
+package es.um.asio.abstractions.domain;
+
+import es.um.asio.abstractions.domain.Operation;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+// ** @JsonIgnoreProperties(ignoreUnknown=true)
+public class ManagementBusEvent<Model> {
+
+	// ** @JsonIgnoreProperties(ignoreUnknown=true)
+	private Model model;
+	
+	private Operation operation;
+}
