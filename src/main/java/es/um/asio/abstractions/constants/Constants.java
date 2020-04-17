04383396dcedcc6eb7764dea9cb49b9b608342cd
Alejandro Martino Uria <amartino@izertis.com>
2020-04-17 13:45:47 +0200

    SEMANTMURC-113 constants

--- src/main/java/es/um/asio/abstractions/constants/Constants.java
+++ src/main/java/es/um/asio/abstractions/constants/Constants.java
@@ -7,4 +7,29 @@ public final class Constants {
 		
 	/** The Constant ROOT_URI. */
 	public static final String ROOT_URI = "http://example.org";
+	
+	/** The Constant OBJECT. */
+	public static final String OBJECT = "object";
+	
+	/** The Constant PROPERTY. */
+	public static final String PROPERTY = "property";
+		
+	/** The Constant CLASS_NAME. */
+	public static final String CLASS_NAME = "className";
+	
+	/** The Constant RESOURCE_ID. */
+	public static final String RESOURCE_ID = "resourceID";
+	
+	/** The Constant LANGUAGE. */
+	public static final String LANGUAGE = "language";
+	
+	/** The Constant UNIVERSITY. */
+	public static final String UNIVERSITY = "university";
+	
+	/** The Constant SPANISH_LANGUAGE. */
+	public static final String SPANISH_LANGUAGE = "es";
+	
+	/** The Constant MURCIA_UNIVERSITY. */
+	public static final String MURCIA_UNIVERSITY ="https://www.um.es";
+	
 }
