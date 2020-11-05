62b9a9a853d0aeb8309c1327e1e33cf199c0601f
Alejandro Martino Uria <amartino@izertis.com>
2020-11-05 10:37:35 +0100

    SEMANTMURC-367 refactoring TripleStoragesUtils

--- src/main/java/es/um/asio/abstractions/constants/Constants.java
+++ src/main/java/es/um/asio/abstractions/constants/Constants.java
@@ -1,6 +1,5 @@
 package es.um.asio.abstractions.constants;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Constants.
  */
@@ -15,36 +14,30 @@ public final class Constants {
 	/** The Constant CANONICAL_LANGUAGE_URI. */
 	public static final String CANONICAL_LANGUAGE_URI = "canonicalLanguageURI";
 	
-	/** The Constant LOCAL_URIS. */
-	public static final String LOCAL_URIS = "localURIS";
-	
-	/** The Constant LOCAL_URI. */
-	public static final String LOCAL_URI = "localUri";
-		
 	/** The Constant CANONICAL_PROPERTY. */
 	public static final String CANONICAL_PROPERTY = "canonicalProperty";
 	
 	/** The Constant CANONICAL_URI. */
 	public static final String CANONICAL_URI = "canonicalURI";
+		
+	/** The Constant CLASE. */
+	public static final String CLASE = "clase";
 	
 	/** The Constant CLASS_NAME. */
 	public static final String CLASS = "@class";
 	
-	/** The Constant CLASE. */
-	public static final String CLASE = "clase";
-	
 	/** The Constant DOMAIN. */
 	public static final String DOMAIN = "domain";
-		
+	
 	/** The Constant DOMAIN_VALUE. */
 	public static final String DOMAIN_VALUE = "hercules.org";
-		
+	
 	/** The Constant ENGLISH_LANGUAGE. */
 	public static final String ENGLISH_LANGUAGE = "en-EN";
-	
+		
 	/** The Constant ENTITY_ID. */
 	public static final String ENTITY_ID = "entityId";
-	
+		
 	/** The Constant ID. */
 	public static final String ID = "id";
 	
@@ -54,17 +47,32 @@ public final class Constants {
 	/** The Constant LANGUAGE. */
 	public static final String LANGUAGE = "language";
 	
+	/** The Constant LINKED_TO. */
+	public static final String LINKED_TO = "linkedTo";
+	
+	/** The Constant LOCAL_URI. */
+	public static final String LOCAL_URI = "localURI";
+	
+	/** The Constant LOCAL_URIS. */
+	public static final String LOCAL_URIS = "localURIS";
+	
 	/** The Constant OBJECT. */
 	public static final String OBJECT = "obj";
-	
+			
 	/** The Constant PROPERTY. */
 	public static final String PROPERTY = "property";
-			
+	
+	/** The Constant REFERENCE. */
+	public static final String REFERENCE = "reference";
+	
 	/** The Constant ROOT_URI. */
 	public static final String ROOT_URI = "http://" + Constants.DOMAIN_VALUE;
 	
 	/** The Constant SPANISH_LANGUAGE. */
 	public static final String SPANISH_LANGUAGE = "es-ES";
+	
+	/** The Constant STORAGE_NAME. */
+	public static final String STORAGE_NAME = "storageName";
 	
 	/** The Constant SUBDOMAIN. */
 	public static final String SUBDOMAIN = "subDomain";
@@ -72,21 +80,18 @@ public final class Constants {
 	/** The Constant SUBDOMAIN_VALUE. */
 	public static final String SUBDOMAIN_VALUE = "um";
 	
-	/** The Constant TYPE_REST. */
-	public static final String TYPE_REST = "res";
+	 /** The Constant TRELLIS. */
+ 	public static final String TRELLIS = "trellis";
 	
 	/** The Constant TYPE_CODE. */
 	public static final String TYPE_CODE = "typeCode";
 	
-	/** The Constant STORAGE_NAME. */
-	public static final String STORAGE_NAME = "storageName";
 	
-	/** The Constant REFERENCE. */
-	public static final String REFERENCE = "reference";
+	/** The Constant TYPE_REST. */
+	public static final String TYPE_REST = "res";
 	
-	
-	/** The Constant LINKED_TO. */
-	public static final String LINKED_TO = "linkedTo";
+	/** The Constant WIKIBASE. */
+	public static final String WIKIBASE = "wikibase";
 	
 	
 	
