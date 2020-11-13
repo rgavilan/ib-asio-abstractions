30e76c19cd9db8c6326dcf9751333036b16d5d62
Alejandro Martino Uria <amartino@izertis.com>
2020-11-13 12:26:42 +0100

    SEMANTMURC-381 improve the performance

--- src/main/java/es/um/asio/abstractions/constants/Constants.java
+++ src/main/java/es/um/asio/abstractions/constants/Constants.java
@@ -5,6 +5,12 @@ package es.um.asio.abstractions.constants;
  */
 public final class Constants {
 		
+	/** The Constant CACHE_ENTITIES. */
+	public static final String CACHE_ENTITIES = "cacheEntities";
+	
+	/** The Constant CACHE_PROPERTIES. */
+	public static final String CACHE_PROPERTIES = "cacheProperties";
+	
 	/** The Constant CANONICAL_CLASS. */
 	public static final String CANONICAL_CLASS = "canonicalClass";
 	
@@ -13,13 +19,13 @@ public final class Constants {
 	
 	/** The Constant CANONICAL_LANGUAGE_URI. */
 	public static final String CANONICAL_LANGUAGE_URI = "canonicalLanguageURI";
-	
+		
 	/** The Constant CANONICAL_PROPERTY. */
 	public static final String CANONICAL_PROPERTY = "canonicalProperty";
 	
 	/** The Constant CANONICAL_URI. */
 	public static final String CANONICAL_URI = "canonicalURI";
-		
+	
 	/** The Constant CLASE. */
 	public static final String CLASE = "clase";
 	
@@ -28,18 +34,33 @@ public final class Constants {
 	
 	/** The Constant DOMAIN. */
 	public static final String DOMAIN = "domain";
-	
+		
 	/** The Constant DOMAIN_VALUE. */
 	public static final String DOMAIN_VALUE = "hercules.org";
+		
+	/**
+	 * The Constant END_LINK_PLAIN
+	 */
+	public final static String END_LINK_PLAIN = "END-LINK-OBJECTS";
+	
+	/**
+	 * The Constant END_PLAIN
+	 */
+	public final static String END_PLAIN = "END-PLAIN-OBJECTS";
 	
 	/** The Constant ENGLISH_LANGUAGE. */
 	public static final String ENGLISH_LANGUAGE = "en-EN";
-		
+	
 	/** The Constant ENTITY_ID. */
 	public static final String ENTITY_ID = "entityId";
-		
+	
 	/** The Constant ID. */
 	public static final String ID = "id";
+	
+	/**
+	 * The Constant KO
+	 */
+	public static final String KO = "KO";
 	
 	/** The Constant LANG. */
 	public static final String LANG = "lang";
@@ -47,22 +68,43 @@ public final class Constants {
 	/** The Constant LANGUAGE. */
 	public static final String LANGUAGE = "language";
 	
+	public static final String LINKED_MODEL = "linkedModel";
+			
 	/** The Constant LINKED_TO. */
 	public static final String LINKED_TO = "linkedTo";
 	
-	public static final String LINKED_MODEL = "linkedModel";
+	public static final String LOCAL_Uri = "localUri";
 	
 	/** The Constant LOCAL_URI. */
 	public static final String LOCAL_URI = "localURI";
 	
-	public static final String LOCAL_Uri = "localUri";
-	
 	/** The Constant LOCAL_URIS. */
 	public static final String LOCAL_URIS = "localURIS";
 	
+	/**
+	 * The Constant MANAGEMENT_SYSTEM_BUSY
+	 */
+	public static final String MANAGEMENT_SYSTEM_BUSY = "No se ha podido realizar la importación, management system en uso por la ETL";
+	
 	/** The Constant OBJECT. */
 	public static final String OBJECT = "obj";
-			
+	
+	/**
+	 * The Constant OK
+	 */
+	public static final String OK = "OK";
+	
+	 /**
+	 * The Constant POJO_FACTORY
+	 */
+	public static final String  POJO_FACTORY= "pojoKafkaListenerContainerFactory";
+	
+	/**
+	 * The Constant POJO_LINK_FACTORY
+	 */
+	public static final String  POJO_LINK_FACTORY= "pojoLinkKafkaListenerContainerFactory";
+	
+	
 	/** The Constant PROPERTY. */
 	public static final String PROPERTY = "property";
 	
@@ -75,70 +117,34 @@ public final class Constants {
 	/** The Constant SPANISH_LANGUAGE. */
 	public static final String SPANISH_LANGUAGE = "es-ES";
 	
-	/** The Constant STORAGE_NAME. */
-	public static final String STORAGE_NAME = "storageName";
-	
-	/** The Constant SUBDOMAIN. */
-	public static final String SUBDOMAIN = "subDomain";
-	
-	/** The Constant SUBDOMAIN_VALUE. */
-	public static final String SUBDOMAIN_VALUE = "um";
-	
-	 /** The Constant TRELLIS. */
- 	public static final String TRELLIS = "trellis";
-	
-	/** The Constant TYPE_CODE. */
-	public static final String TYPE_CODE = "typeCode";
-	
-	
-	/** The Constant TYPE_REST. */
-	public static final String TYPE_REST = "res";
-	
-	/** The Constant WIKIBASE. */
-	public static final String WIKIBASE = "wikibase";
-	
-	/**
-	 * The Constant POJO_LINK_FACTORY
-	 */
-	public static final String  POJO_LINK_FACTORY= "pojoLinkKafkaListenerContainerFactory";
-	
-	/**
-	 * The Constant POJO_FACTORY
-	 */
-	public static final String  POJO_FACTORY= "pojoKafkaListenerContainerFactory";
-	
-	/**
-	 * The Constant START_PLAIN
-	 */
-	public final static String START_PLAIN = "START-PLAIN-OBJECTS";
-	
-	/**
-	 * The Constant END_PLAIN
-	 */
-	public final static String END_PLAIN = "END-PLAIN-OBJECTS";
-	
 	/**
 	 * The Constant START_LINK_PLAIN
 	 */
 	public final static String START_LINK_PLAIN = "START-LINK-OBJECTS";
 	
 	/**
-	 * The Constant END_LINK_PLAIN
+	 * The Constant START_PLAIN
 	 */
-	public final static String END_LINK_PLAIN = "END-LINK-OBJECTS";
+	public final static String START_PLAIN = "START-PLAIN-OBJECTS";
+	
+	/** The Constant STORAGE_NAME. */
+	public static final String STORAGE_NAME = "storageName";
+	
+	/** The Constant SUBDOMAIN. */
+	public static final String SUBDOMAIN = "subDomain";
 
-	/**
-	 * The Constant KO
-	 */
-	public static final String KO = "KO";
+	/** The Constant SUBDOMAIN_VALUE. */
+	public static final String SUBDOMAIN_VALUE = "um";
 	
-	/**
-	 * The Constant MANAGEMENT_SYSTEM_BUSY
-	 */
-	public static final String MANAGEMENT_SYSTEM_BUSY = "No se ha podido realizar la importación, management system en uso por la ETL";
+	/** The Constant TRELLIS. */
+ 	public static final String TRELLIS = "trellis";
 	
-	/**
-	 * The Constant OK
-	 */
-	public static final String OK = "OK";
+	/** The Constant TYPE_CODE. */
+	public static final String TYPE_CODE = "typeCode";
+	
+	/** The Constant TYPE_REST. */
+	public static final String TYPE_REST = "res";
+	
+	/** The Constant WIKIBASE. */
+	public static final String WIKIBASE = "wikibase";
 }
